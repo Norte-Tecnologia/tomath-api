a349246b25fa4b2ca5b1456bcb5adc83c80c527f
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-02-23 20:22:44 -0300

    migrando projeto

--- src/main/kotlin/com/tomath/cliente/ClienteRepositorio.kt
+++ src/main/kotlin/com/tomath/cliente/ClienteRepositorio.kt
@@ -1,6 +1,6 @@
 package com.tomath.cliente
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
