6b955e07df1e2aa63442b602ded5087378c7cf8b
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-02-25 20:58:32 -0300

    refatorando classe

--- src/main/kotlin/com/tomath/cliente/Cliente.kt
+++ src/main/kotlin/com/tomath/cliente/Cliente.kt
@@ -1,14 +1,27 @@
 package com.tomath.cliente
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
+import org.hibernate.Hibernate
 
 @Entity
-class Cliente {
+data class Cliente (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
+    var id: Long? = null,
     var nome: String? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Cliente
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id , nome = $nome )"
+    }
 }
