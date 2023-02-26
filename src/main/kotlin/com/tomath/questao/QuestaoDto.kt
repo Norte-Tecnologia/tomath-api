600017e15c8dbb9c4314d64b22c4976090cd5017
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-02-26 14:49:15 -0300

    criando dto

--- src/main/kotlin/com/tomath/questao/QuestaoDto.kt
+++ src/main/kotlin/com/tomath/questao/QuestaoDto.kt
@@ -3,7 +3,5 @@ package com.tomath.questao
 import java.io.Serializable
 
 data class QuestaoDto(
-    val materia: String,
-    val comando: String,
-    val resposta: String
+    val materia: String
 ) : Serializable
