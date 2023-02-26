7b203837df98410c2896591382d220e68e7fc9cb
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-02-26 14:49:40 -0300

    removendo null safe

--- src/main/kotlin/com/tomath/questao/QuestaoRepository.kt
+++ src/main/kotlin/com/tomath/questao/QuestaoRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface QuestaoRepository : JpaRepository<Questao?, Long?>
+interface QuestaoRepository : JpaRepository<Questao, Long>
